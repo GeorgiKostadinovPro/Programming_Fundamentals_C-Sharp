cd99db18a67d4e31e7da2349b470912f13260b79
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-25 14:49:03 +0300

    Add files via upload

---
+++ FinalExam03April2022/01_StringGame/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace StringGame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            string line = string.Empty;
+
+            while ((line = Console.ReadLine()) != "Done")
+            {
+                string[] commandArgs = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string command = commandArgs[0];
+
+                if (command == "Change")
+                {
+                    string character = commandArgs[1];
+                    string replacement = commandArgs[2];
+                    
+                    str = str.Replace(character, replacement);
+                    Console.WriteLine(str);
+                }
+                else if (command == "Includes")
+                {
+                    string subStr = commandArgs[1];
+
+                    if (str.Contains(subStr))
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+                else if (command == "End")
+                {
+                    string subStr = commandArgs[1];
+
+                    if (str.EndsWith(subStr))
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+                else if (command == "Uppercase")
+                {
+                    str = str.ToUpper();
+                    Console.WriteLine(str);
+                }
+                else if (command == "FindIndex")
+                {
+                    string character = commandArgs[1];
+
+                    int index = str.IndexOf(character);
+                    Console.WriteLine(index);
+                }
+                else if (command == "Cut")
+                {
+                    int startIdx = int.Parse(commandArgs[1]);
+                    int count = int.Parse(commandArgs[2]);
+
+                    string subStr = str.Substring(startIdx, count);
+
+                    str = subStr;
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
