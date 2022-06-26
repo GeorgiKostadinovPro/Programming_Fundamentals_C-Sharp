f4b6940a8306e7217a780fc091373904aacec09d
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-26 19:09:01 +0300

    Add files via upload

---
+++ BasicSyntaxConditionalStatementsAndLoops/Lab/05_MonthPrinter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05_MonthPrinter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int month = int.Parse(Console.ReadLine());
+
+            if (month > 0 && month <= 12)
+            {
+                DateTime dt = new DateTime(1, month, 1);
+                Console.WriteLine(dt.ToString("MMMM"));
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
