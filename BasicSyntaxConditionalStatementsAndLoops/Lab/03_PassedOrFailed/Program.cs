029793bbd061c5e90f39d1b074b1fb72fdca391b
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-26 19:08:26 +0300

    Add files via upload

---
+++ BasicSyntaxConditionalStatementsAndLoops/Lab/03_PassedOrFailed/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03_PassedOrFailed
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            string result = grade >= 3.00 ? "Passed!" : "Failed!";
+
+            Console.WriteLine(result);
+        }
+    }
+}
