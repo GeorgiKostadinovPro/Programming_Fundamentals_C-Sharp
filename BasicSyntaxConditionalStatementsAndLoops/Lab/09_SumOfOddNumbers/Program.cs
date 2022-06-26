6bd61f0b8a38689bbf3572d6fe8b52673c1419a8
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-26 19:09:28 +0300

    Add files via upload

---
+++ BasicSyntaxConditionalStatementsAndLoops/Lab/09_SumOfOddNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _09_SumOfOddNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int countOfOddNumber = 1;
+            int sum = 1;
+            int j = 2;
+            Console.WriteLine("1");
+
+            while (true)
+            {
+                if (j % 2 == 1)
+                {
+                    Console.WriteLine($"{j}");
+                    countOfOddNumber++;
+                    sum += j;
+                    j++;
+                }
+                else
+                {
+                    j++;
+                }
+
+                if (countOfOddNumber == n)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
