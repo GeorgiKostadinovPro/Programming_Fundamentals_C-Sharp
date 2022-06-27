35cb50ec922d7ec0b6a90e023c012f7c759e0d51
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-27 12:45:04 +0300

    Add files via upload

---
+++ BasicSyntaxConditionalStatementsAndLoops/Exercises/06_StrongNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06_StrongNumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                int digit = n[i] - '0';
+                int digitFactoriel = Factoriel(digit);
+                sum += digitFactoriel;
+            }
+
+            if (sum == int.Parse(n))
+            {
+                Console.WriteLine("yes");
+            }
+            else 
+            {
+                Console.WriteLine("no");
+            }
+        }
+
+        private static int Factoriel(int n)
+        {
+            if (n < 1)
+            {
+                return 1;
+            }
+
+            return n * Factoriel(n - 1);
+        }
+    }
+}
