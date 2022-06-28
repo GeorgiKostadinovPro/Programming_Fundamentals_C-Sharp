63c471b42c5dc4135ccb660d5b3e292157e007be
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-06-28 20:35:10 +0300

    Add files via upload

---
+++ BasicSyntaxConditionalStatementsAndLoops/MoreExercises/01_SortNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_SortNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            double[] arr = { a, b, c};
+
+            foreach (var item in arr.OrderByDescending(x => x))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
