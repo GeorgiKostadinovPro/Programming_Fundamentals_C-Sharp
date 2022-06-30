0be370a4ba0b2c6a11eca8333efbcc9dbb0bf1fc
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-06-30 14:22:54 +0300

    Add files via upload

---
+++ DataTypesAndVariables/Exercises/09_SpiceMustFlow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _09_SpiceMustFlow
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());
+            int collect = 0;
+            int days = 0;
+
+            if (yield >= 100)
+            {
+                while (yield >= 100)
+                {
+                    days++;
+                    collect += yield;
+                    yield -= 10;
+
+                }
+                collect -= (days + 1) * 26;
+                Console.WriteLine(days);
+                Console.WriteLine(collect);
+            }
+            else
+            {
+                Console.WriteLine(days);
+                Console.WriteLine(collect);
+            }
+        }
+    }
+}
