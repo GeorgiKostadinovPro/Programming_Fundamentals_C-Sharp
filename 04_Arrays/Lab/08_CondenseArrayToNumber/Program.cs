6286722aaa210b09765b2fa0f14dc2cfb3df9368
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-03 22:55:35 +0300

    Add files via upload

---
+++ 04_Arrays/Lab/08_CondenseArrayToNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _08_CondenseArrayToNumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var count = nums.Length;
+
+            while (count > 1)
+            {
+                for (int i = 0; i < count - 1; i++)
+                {
+                    nums[i] = nums[i] + nums[i + 1];
+                }
+
+                count--;
+            }
+
+            Console.WriteLine(nums[0]);
+        }
+    }
+}
