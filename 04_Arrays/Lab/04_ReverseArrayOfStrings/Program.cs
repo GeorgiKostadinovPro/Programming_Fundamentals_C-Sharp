4180e4af2c1237e4ba81b7f08f53f5cd815e2687
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-03 22:53:43 +0300

    Add files via upload

---
+++ 04_Arrays/Lab/04_ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04_ReverseArrayOfStrings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] nums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Array.Reverse(nums);
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
