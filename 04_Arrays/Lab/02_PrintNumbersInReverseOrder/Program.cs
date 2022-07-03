c3f7a49ae3ab019037f6eb0c6c49fa9ba41dc4cf
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-03 22:53:03 +0300

    Add files via upload

---
+++ 04_Arrays/Lab/02_PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02_PrintNumbersInReverseOrder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = new int[n];
+
+            while (n > 0)
+            {
+                int num = int.Parse(Console.ReadLine());
+                nums[n - 1] = num;
+                n--;
+            }
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
