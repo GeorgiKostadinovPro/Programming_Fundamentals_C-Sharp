3c623da6cb6b85f68658d0026c5ff4415ad3eeb4
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-03 22:53:25 +0300

    Add files via upload

---
+++ 04_Arrays/Lab/03_RoundingNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_RoundingNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                double roundedNumber = Math.Round(nums[i], 0, MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{nums[i]} => {Convert.ToDecimal(roundedNumber)}");
+            }
+        }
+    }
+}
