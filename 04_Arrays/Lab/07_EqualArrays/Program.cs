ec7810f84ee5376915ef514a88dfd31b47f6269e
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-03 22:55:21 +0300

    Add files via upload

---
+++ 04_Arrays/Lab/07_EqualArrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _07_EqualArrays
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] first = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+
+            int[] second = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            bool areEqual = true;
+            int index = 0;
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (first[i] != second[i])
+                {
+                    areEqual = false;
+                    index = i;
+                    break;
+                }
+            }
+
+            if (areEqual)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {first.Sum()}");
+            }
+            else 
+            {
+                Console.WriteLine($"Arrays are not identical. Found difference at {index} index");
+            }
+        }
+    }
+}
