2b3cf44f85f4c62ae97c463331c867d1382dff7d
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-03 22:54:19 +0300

    Add files via upload

---
+++ 04_Arrays/Lab/05_SumEvenNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _05_SumEvenNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    sum += nums[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
