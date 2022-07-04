e1e5dbed6fef5c17fc6db2d71ea03dc27ef92b91
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-04 20:12:10 +0300

    Add files via upload

---
+++ 04_Arrays/Exercises/01_Train/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_Train
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] train = new int[n];
+            int totalPassengers = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int numberOfPeople = int.Parse(Console.ReadLine());
+                train[i] += numberOfPeople;
+                totalPassengers += numberOfPeople;
+            }
+
+            Console.WriteLine(string.Join(" ", train));
+            Console.WriteLine(totalPassengers);
+        }
+    }
+}
---
+++ 04_Arrays/Exercises/02_CommonElements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02_CommonElements
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] first = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] second = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < second.Length; i++)
+            {
+                for (int j = 0; j < first.Length; j++)
+                {
+                    if (second[i] == first[j])
+                    {
+                        Console.Write(second[i] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
