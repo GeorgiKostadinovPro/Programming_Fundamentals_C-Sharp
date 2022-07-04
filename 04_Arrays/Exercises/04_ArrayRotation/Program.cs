be01b24cd8dcee0c103e0f419adf36ebe35f3b88
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-04 20:12:55 +0300

    Add files via upload

---
+++ 04_Arrays/Exercises/04_ArrayRotation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _04_ArrayRotation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+           
+            int rotations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rotations; i++)
+            {
+                int first = numbers[0];
+
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    numbers[j] = numbers[j + 1];
+                }
+
+                numbers[numbers.Length - 1] = first;
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
