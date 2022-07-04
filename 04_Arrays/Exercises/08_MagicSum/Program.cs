e8c3b44860dc4f405899b0355f1c5434bffa7c9c
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-04 20:13:41 +0300

    Add files via upload

---
+++ 04_Arrays/Exercises/08_MagicSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _08_MagicSum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                     .Select(int.Parse)
+                     .ToArray();
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int firstElement = numbers[i];
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int secondElement = numbers[j];
+                    int sum = firstElement + secondElement;
+
+                    if (sum == number)
+                    {
+                        Console.WriteLine(firstElement + " " + secondElement);
+                    }
+                }
+            }
+        }
+    }
+}
