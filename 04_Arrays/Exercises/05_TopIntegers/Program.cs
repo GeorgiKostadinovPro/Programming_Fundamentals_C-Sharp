41bb39362ec07ae3fed63ce5933988ab96171f74
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-04 20:13:08 +0300

    Add files via upload

---
+++ 04_Arrays/Exercises/05_TopIntegers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _05_TopIntegers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                 bool isTopInteger = true;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] <= numbers[j])
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+
+                if (isTopInteger)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+        }
+    }
+}
