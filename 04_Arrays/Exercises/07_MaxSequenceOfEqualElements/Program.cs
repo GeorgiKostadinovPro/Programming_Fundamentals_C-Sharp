86921ec8dc3bf2843a0e303a8a78f17bdfb475a9
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-04 20:13:31 +0300

    Add files via upload

---
+++ 04_Arrays/Exercises/07_MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _07_MaxSequenceOfEqualElements
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int count = 1;
+            int theBestCount = 0;
+            int element = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 1;
+                }
+
+                if (count > theBestCount)
+                {
+                    theBestCount = count;
+                    element = numbers[i];
+                }
+            }
+
+            for (int j = 0; j < theBestCount; j++)
+            {
+                Console.Write($"{element} ");
+            }
+        }
+    }
+}
