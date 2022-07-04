ccc3a5668df6bbe13c670248c556852c60a6edd8
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-04 20:13:18 +0300

    Add files via upload

---
+++ 04_Arrays/Exercises/06_EqualSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_EqualSum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+            bool isFound = false;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int rightSum = numbers.Skip(i + 1).Sum();
+                int leftSum = numbers.Take(i).Sum();
+
+                if (rightSum == leftSum)
+                {
+                    Console.WriteLine(i);
+                    isFound = true;
+                    break;
+                }
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
