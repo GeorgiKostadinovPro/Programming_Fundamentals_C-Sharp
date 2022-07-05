2d2cfd0caf2ec1b7b22359f4fa2bf458211c1fb0
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-06 00:07:48 +0300

    Add files via upload

---
+++ 05_Methods/Lab/09_GreaterOfTwoValues/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09_GreaterOfTwoValues
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputName = Console.ReadLine();
+            string firstName = Console.ReadLine();
+            string secondName = Console.ReadLine();
+
+            Console.WriteLine(GreaterValue(inputName, firstName, secondName));
+        }
+
+        private static string GreaterValue(string inputName, string name1, string name2)
+        {
+            string result = string.Empty;
+
+            switch (inputName)
+            {
+                case "int":
+                    result = (Math.Max(int.Parse(name1), int.Parse(name2))).ToString();
+                    break;
+                case "char":
+                case "string":
+                    return name1[0] > name2[0] ? name1 : name2;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
