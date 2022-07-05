6fcc20e159f4d770be9e8223550da2824f628f1a
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-06 00:04:55 +0300

    Add files via upload

---
+++ 05_Methods/Lab/01_SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01_SignOfIntegerNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string result = NumberSign(number);
+            Console.WriteLine(result);
+        }
+
+        private static string NumberSign(int number)
+        {
+            if (number > 0)
+            {
+                return $"The number {number} is positive. ";
+            }
+            else if (number < 0)
+            {
+                return $"The number {number} is negative. ";
+            }
+
+            return $"The number {number} is zero. ";
+        }
+    }
+}
