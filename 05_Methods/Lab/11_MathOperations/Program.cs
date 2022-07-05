2350dd9ab990fa8d65276fdeaf5827d1a2a9bcb5
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-06 00:08:10 +0300

    Add files via upload

---
+++ 05_Methods/Lab/11_MathOperations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _11_MathOperations
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            char @operator = char.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            double result = Calculate(firstNumber, secondNumber, @operator);
+            Console.WriteLine(result);
+        }
+
+        private static double Calculate(double firstNumber, double secondNumber, char @operator)
+        {
+            if (@operator == '+')
+            {
+                return firstNumber + secondNumber;
+            }
+            else if (@operator == '-')
+            {
+                return firstNumber - secondNumber;
+            }
+            else if (@operator == '*')
+            {
+                return firstNumber * secondNumber;
+            }
+
+            return firstNumber / secondNumber;
+        }
+    }
+}
