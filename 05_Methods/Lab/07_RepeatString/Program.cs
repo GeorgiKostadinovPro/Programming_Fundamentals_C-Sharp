bd9df005f806569f0568f141a54acdf3691cd085
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-06 00:06:42 +0300

    Add files via upload

---
+++ 05_Methods/Lab/07_RepeatString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07_RepeatString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            string result = RepeatString(str, n);
+            Console.WriteLine(result);
+        }
+
+        private static string RepeatString(string str, int times)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < times; i++)
+            {
+                result += str;
+            }
+
+            return result;
+        }
+    }
+}
