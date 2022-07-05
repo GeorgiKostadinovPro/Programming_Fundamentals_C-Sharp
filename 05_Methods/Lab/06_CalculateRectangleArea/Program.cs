d6ac483ca630c53f8041580234b5630009b01190
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-06 00:06:28 +0300

    Add files via upload

---
+++ 05_Methods/Lab/06_CalculateRectangleArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06_CalculateRectangleArea
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           double width = double.Parse(Console.ReadLine());   
+           double height = double.Parse(Console.ReadLine());
+
+           double result = GetArea(width, height);
+           Console.WriteLine(result);
+        }
+        private static double GetArea(double width, double height)
+        {
+           return width * height;
+        }
+    }
+}
