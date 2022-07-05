bcf45463ae1f8a52670bbf16b652648de9fe6847
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-06 00:05:34 +0300

    Add files via upload

---
+++ 05_Methods/Lab/03_Calculations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _03_Calculations
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            string result = NumberSign(grade);
+            Console.WriteLine(result);
+        }
+
+        private static string NumberSign(double grade)
+        {
+            if (grade >= 5.5 && grade <= 6)
+            {
+                return "Excellent";
+            }
+            else if (grade >= 4.5 && grade < 5.5)
+            {
+                return "Very good";
+            }
+            else if (grade >= 3.5 && grade < 4.5)
+            {
+                return "Good";
+            }
+            else if (grade >= 3 && grade < 3.5)
+            {
+                return "Poor";
+            }
+
+            return "Fail";
+        }
+    }
+}
---
+++ 05_Methods/Lab/04_PrintingTriangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04_PrintingTriangle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintTriangle(n);
+        }
+
+        private static void PrintTriangle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
