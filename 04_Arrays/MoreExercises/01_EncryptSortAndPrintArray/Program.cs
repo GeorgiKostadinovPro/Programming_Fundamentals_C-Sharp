233a80c050aa586c46a5bbf92e37bc3abdc3b85c
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-05 11:50:12 +0300

    Add files via upload

---
+++ 04_Arrays/MoreExercises/01_EncryptSortAndPrintArray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_EncryptSortAndPrintArray
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] result = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string str = Console.ReadLine();
+                int strSum = 0;
+
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str[j] == 'a' || str[j] == 'e'
+                        || str[j] == 'i' || str[j] == 'o' || str[j] == 'u'
+                        || str[j] == 'A' || str[j] == 'E'
+                        || str[j] == 'I' || str[j] == 'O' || str[j] == 'U')
+                    {
+                        strSum += (int)str[j] * str.Length;
+                    }
+                    else 
+                    { 
+                        strSum += (int)str[j] / str.Length;
+                    }
+                }
+
+                result[i] = strSum;
+            }
+
+            result = result.OrderBy(e => e).ToArray();
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+    }
+}
---
+++ 04_Arrays/MoreExercises/02_PascalTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02_PascalTriangle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[][] pascalTriangle = new long[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                pascalTriangle[i] = new long[i + 1];
+                pascalTriangle[i][0] = 1;
+                pascalTriangle[i][^1] = 1;
+
+                for (int j = 1; j < i; j++)
+                {
+                    pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(string.Join(" ", pascalTriangle[i]));
+            }
+        }
+    }
+}
