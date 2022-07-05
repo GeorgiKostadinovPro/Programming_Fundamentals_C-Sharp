029b935086fbc7a203d40b289fd71b564fdde45e
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-05 13:38:02 +0300

    Add files via upload

---
+++ 04_Arrays/MoreExercises/03_RecursiveFibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03_RecursiveFibonacci
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int result = Fibonacci(n);
+            Console.WriteLine(result);
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
