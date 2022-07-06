59ef1060f65984eb90c5b355869a94cd0b9f8418
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-07 00:03:09 +0300

    Add files via upload

---
+++ 05_Methods/Exercises/06_MiddleCharacters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06_MiddleCharacters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            PrintMiddleCharacters(str);
+        }
+
+        private static void PrintMiddleCharacters(string str)
+        {
+            string result = string.Empty;
+            int middle = str.Length / 2;
+
+            if (str.Length % 2 != 0)
+            {
+                result += str[middle];
+            }
+            else
+            {
+                result += str[middle - 1];
+                result += str[middle];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
