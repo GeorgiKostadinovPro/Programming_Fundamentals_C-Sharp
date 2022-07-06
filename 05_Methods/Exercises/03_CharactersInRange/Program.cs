a66fce2595c47b072374b287c733498eccec7c1f
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-07 00:02:42 +0300

    Add files via upload

---
+++ 05_Methods/Exercises/03_CharactersInRange/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _03_CharactersInRange
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+
+            GetCharactersInRange(start, end);
+        }
+
+        private static void GetCharactersInRange(char start, char end)
+        {
+            if ((int)start > (int)end)
+            {
+                char temp = start;
+                start = end;
+                end = temp;
+            }
+
+            for (int i = start + 1; i < end; i++)
+            {
+                Console.Write(Convert.ToChar(i) + " ");
+            }
+        }
+    }
+}
