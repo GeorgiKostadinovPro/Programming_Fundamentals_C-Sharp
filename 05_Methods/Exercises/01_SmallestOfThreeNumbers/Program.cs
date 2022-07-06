cfffa5463711af233cdcf6ddbe642195c2bc118b
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-07 00:02:24 +0300

    Add files via upload

---
+++ 05_Methods/Exercises/01_SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01_SmallestOfThreeNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine()); 
+            int third = int.Parse(Console.ReadLine());
+
+            SmallestNumber(first, second, third);
+        }
+
+        private static void SmallestNumber(int first, int second, int third)
+        {
+            Console.WriteLine(Math.Min(first, Math.Min(second, third)));
+        }
+    }
+}
---
+++ 05_Methods/Exercises/02_VowelsCount/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02_VowelsCount
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            GetVowelsCountInString(str);
+        }
+
+        private static void GetVowelsCountInString(string str)
+        {
+            int vowelsCount = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' 
+                    || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'
+                    || str[i] == 'A' || str[i] == 'E' 
+                    || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+                {
+                    vowelsCount++;
+                }
+            }
+
+            Console.WriteLine(vowelsCount);
+        }
+    }
+}
