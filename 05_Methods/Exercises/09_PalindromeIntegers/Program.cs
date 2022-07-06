f1391b850e075ad3411c02b9dcdb5f93d033659a
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-07 00:03:46 +0300

    Add files via upload

---
+++ 05_Methods/Exercises/09_PalindromeIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _09_PalindromeIntegers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string line = string.Empty;
+
+            while ((line = Console.ReadLine()) != "END")
+            {
+                IsPalindrome(line);
+            }
+        }
+
+        private static void IsPalindrome(string line)
+        {
+            string str = string.Empty;
+
+            for (int i = line.Length - 1; i >= 0; i--)
+            {
+                str += line[i];
+            }
+
+            if (line == str)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
