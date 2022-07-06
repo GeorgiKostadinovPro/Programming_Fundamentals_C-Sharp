2b2d58da102c6c1b19371df869bb9a542dd33c9f
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-07 00:03:34 +0300

    Add files via upload

---
+++ 05_Methods/Exercises/08_FactorialDivision/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08_FactorialDivision
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double first = double.Parse(Console.ReadLine());
+            double second = double.Parse(Console.ReadLine());
+
+            double firstFactorial = Factorial(first);
+            double secondFactorial = Factorial(second);
+
+            double result = firstFactorial / secondFactorial;
+            Console.WriteLine($"{result:f2}");
+        }
+
+        private static double Factorial(double n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
