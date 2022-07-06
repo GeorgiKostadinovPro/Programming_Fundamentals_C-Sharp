468a860f3298284dbd1824af0c97187f94f3a3a8
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-07 00:02:57 +0300

    Add files via upload

---
+++ 05_Methods/Exercises/04_PasswordValidator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _04_PasswordValidator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            IsValid(password);
+        }
+
+        private static void IsValid(string password)
+        {
+            bool isValid = true;
+
+            if (password.Length < 6 || password.Length > 10)
+            {
+                isValid = false;
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+
+            if (password.Any(c => !char.IsLetterOrDigit(c)))
+            {
+                isValid = false;
+                Console.WriteLine( "Password must consist only of letters and digits");
+            }
+
+            if (password.Count(c => char.IsDigit(c)) < 2)
+            {
+                isValid = false;
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+    }
+}
---
+++ 05_Methods/Exercises/05_AddAndSubtract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05_AddAndSubtract
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int sum = Add(first, second);
+            int subtraction = Subtract(sum, third);
+
+            Console.WriteLine(subtraction);
+        }
+
+        private static int Subtract(int sum, int third)
+        {
+            return sum - third; 
+        }
+
+        private static int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
