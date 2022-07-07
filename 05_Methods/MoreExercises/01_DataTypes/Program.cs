d298d63fb30b91354294b6147f0cf66ba4520548
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-08 00:03:29 +0300

    Add files via upload

---
+++ 05_Methods/MoreExercises/01_DataTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01_DataTypes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            string line = Console.ReadLine();
+
+            DataTypes(type, line);
+        }
+
+        private static void DataTypes(string type, string line)
+        {
+            if (type == "int")
+            {
+                int number = int.Parse(line) * 2;
+                Console.WriteLine(number);
+            }
+            else if (type == "real")
+            {
+                double number = double.Parse(line) * 1.5;
+                Console.WriteLine($"{number:f2}");
+            }
+            else if (type == "string")
+            {
+                string str = "$" + line + "$";
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
---
+++ 05_Methods/MoreExercises/02_CenterPoint/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02_CenterPoint
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            ClosestPoint(x1, y1, x2, y2);
+        }
+
+        private static void ClosestPoint(double x1, double y1, double x2, double y2)
+        {
+            double centerX = 0;
+            double centerY = 0;
+
+            double firstPointDistance = Math.Sqrt(Math.Pow(x1 - centerX, 2) + Math.Pow(y1 - centerY, 2));
+            double secondPointDistance = Math.Sqrt(Math.Pow(x2 - centerX, 2) + Math.Pow(y2 - centerY, 2));
+
+            if (firstPointDistance <= secondPointDistance)
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else
+            {
+               Console.WriteLine($"({x2}, {y2})");
+            }
+        }
+    }
+}
