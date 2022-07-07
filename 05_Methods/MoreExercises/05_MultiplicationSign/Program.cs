26e05c4ee87e19bf0f073cead92659a59735443b
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-08 00:04:07 +0300

    Add files via upload

---
+++ 05_Methods/MoreExercises/05_MultiplicationSign/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _05_MultiplicationSign
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int[] numbers = new[] { first, second, third };
+            IsPosititveNegativeOrZeroSign(numbers);
+        }
+
+        private static void IsPosititveNegativeOrZeroSign(int[] numbers)
+        {
+            int countNeg = 0;
+            int countZero = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    countNeg++;
+                }
+                else if (numbers[i] == 0)
+                {
+                    countZero++;
+                }
+            }
+
+            if (countZero > 0)
+            {
+                Console.WriteLine("zero");
+                return;
+            }
+
+            if (countNeg % 2 != 0)
+            {
+                Console.WriteLine("negative");
+                return;
+            }
+
+            Console.WriteLine("positive");
+        }
+    }
+}
