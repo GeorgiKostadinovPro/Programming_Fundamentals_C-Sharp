9d61f09aa7dc004774c6ae49fececd7fe8f32f85
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-09 22:23:45 +0300

    Add files via upload

---
+++ 06_List/Lab/03_MergingLists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_MergingLists
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<double> first = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            List<double> second = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(double.Parse)
+               .ToList();
+
+            List<double> result = new List<double>();
+
+            while (first.Count > 0 && second.Count > 0)
+            {
+                result.Add(first[0]);
+                result.Add(second[0]);
+                first.RemoveAt(0);
+                second.RemoveAt(0);
+            }
+
+            if (first.Count > 0)
+            {
+                result.AddRange(first);
+            }
+            else 
+            {
+                result.AddRange(second);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
