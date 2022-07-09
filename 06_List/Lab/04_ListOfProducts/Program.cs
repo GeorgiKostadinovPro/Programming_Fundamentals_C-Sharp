4e7ca0e2bc8db030f8176089f24b5475bc305fd3
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-09 22:23:56 +0300

    Add files via upload

---
+++ 06_List/Lab/04_ListOfProducts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_ListOfProducts
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string product = Console.ReadLine();
+                products.Add(product);
+            }
+
+            products = products.OrderBy(p => p).ToList();
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }    
+        }
+    }
+}
