82d2caacdd6af85b3642fde23148cf82bb636713
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-09 22:23:27 +0300

    Add files via upload

---
+++ 06_List/Lab/02_GaussTrick/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_GaussTrick
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<decimal> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (i == numbers.Count - 1)
+                {
+                    break;
+                }
+
+                numbers[i] += numbers[numbers.Count - 1];
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
