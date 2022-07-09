947b29475047dade9cabb4eeb7038ddd307bead1
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-09 22:24:09 +0300

    Add files via upload

---
+++ 06_List/Lab/05_RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_RemoveNegativesAndReverse
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries) 
+                .Select(int.Parse)
+                .ToList();
+
+            numbers.RemoveAll(x => x < 0);
+
+            string result = numbers.Count > 0 ? string.Join(" ", numbers.Reverse<int>()) : "empty";
+            Console.WriteLine(result);
+        }
+    }
+}
