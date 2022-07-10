b13183dd6b7623654eb74190a5da26cadac5e4c8
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-10 15:28:30 +0300

    Add files via upload

---
+++ 06_List/Exercises/03_HouseParty/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_HouseParty
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> guestsList = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArgs = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string guestName = cmdArgs[0];
+
+                if (cmdArgs.Length == 3)
+                {
+                    if (guestsList.Contains(guestName))
+                    {
+                        Console.WriteLine($"{guestName} is already in the list!");
+                        continue;
+                    }
+
+                    guestsList.Add(guestName);
+                }
+                else 
+                {
+                    if (!guestsList.Contains(guestName))
+                    {
+                        Console.WriteLine($"{guestName} is not in the list!");
+                        continue;
+                    }
+
+                    guestsList.Remove(guestName);
+                }
+            }
+
+            guestsList.ForEach(Console.WriteLine);
+        }
+    }
+}
