0de9185aff871399c412168167395eeb1dccafa1
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-10 15:35:25 +0300

    Add files via upload

---
+++ 06_List/Exercises/07_AppendArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07_AppendArrays
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> numbers = new List<string>();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                string[] elements = input[i]
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                numbers.AddRange(elements);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
