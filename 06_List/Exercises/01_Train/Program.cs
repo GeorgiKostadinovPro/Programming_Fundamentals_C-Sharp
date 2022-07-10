50a1b167c5f377cb95a615a016222cd3d062a718
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-10 15:28:15 +0300

    Add files via upload

---
+++ 06_List/Exercises/01_Train/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Train
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string line = string.Empty;
+
+            while ((line = Console.ReadLine()) != "end")
+            {
+                string[] cmdArgs = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (cmdArgs.Length == 2)
+                {
+                    int passengers = int.Parse(cmdArgs[1]);
+                    wagons.Add(passengers);
+                }
+                else 
+                {
+                    int passengers = int.Parse(cmdArgs[0]);
+
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i] + passengers <= maxCapacity)
+                        {
+                            wagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
---
+++ 06_List/Exercises/02_ChangeList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_ChangeList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string line = string.Empty;
+
+            while ((line = Console.ReadLine()) != "end")
+            {
+                string[] cmdArgs = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string cmd = cmdArgs[0];
+                int element = int.Parse(cmdArgs[1]);
+
+                if (cmd == "Delete")
+                {
+                    numbers.RemoveAll(x => x == element);
+                }
+                else 
+                { 
+                    int position = int.Parse(cmdArgs[2]);
+                    numbers.Insert(position, element);
+                } 
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
