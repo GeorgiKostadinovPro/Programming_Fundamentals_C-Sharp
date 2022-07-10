3d1a893b2b88b156f7ce2334b561e4aa4f969ea5
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-10 15:35:04 +0300

    Add files via upload

---
+++ 06_List/Exercises/06_CardsGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06_CardsGame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> firstHandOfCards = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondHandOfCards = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToList();
+
+            while (firstHandOfCards.Count > 0 && secondHandOfCards.Count > 0)
+            {
+                int firstPlayerCard = firstHandOfCards[0];
+                int secondPlayerCard = secondHandOfCards[0];
+
+                firstHandOfCards.RemoveAt(0);
+                secondHandOfCards.RemoveAt(0);
+
+                if (firstPlayerCard > secondPlayerCard)
+                {
+                    firstHandOfCards.Add(secondPlayerCard);
+                    firstHandOfCards.Add(firstPlayerCard);
+                }
+                else if (firstPlayerCard < secondPlayerCard)
+                {
+                    secondHandOfCards.Add(firstPlayerCard);
+                    secondHandOfCards.Add(secondPlayerCard);
+                }
+            }
+
+            if (firstHandOfCards.Count > 0)
+            {
+                Console.WriteLine($"First player wins! Sum: {firstHandOfCards.Sum()}");
+            }
+            else
+            { 
+                Console.WriteLine($"Second player wins! Sum: {secondHandOfCards.Sum()}");
+            }
+        }
+    }
+}
