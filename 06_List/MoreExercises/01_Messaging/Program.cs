3a6404d6e479c6d9ba925b48f2d94ba175b3a23f
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-11 17:56:33 +0300

    Add files via upload

---
+++ 06_List/MoreExercises/01_Messaging/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01_Messaging
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string str = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder(str);
+
+            string message = string.Empty;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int currElement = numbers[i];
+                int currElementSum = 0;
+
+                while (currElement != 0)
+                {
+                    currElementSum += currElement % 10;
+                    currElement /= 10;
+                }
+
+                char letter = '\u0000';
+
+                if (currElementSum >= str.Length)
+                {
+                    currElementSum -= str.Length;
+                    letter = sb[currElementSum];
+                }
+                else 
+                { 
+                    letter = sb[currElementSum];   
+                }
+                
+                sb.Remove(currElementSum, 1);
+                message += letter;
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ 06_List/MoreExercises/02_CarRace/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02_CarRace
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int midIdx = numbers.Count / 2;
+
+            double firstRacerTime = 0.0D;
+            double secondRacerTime = 0.0D;
+
+            for (int i = 0; i < midIdx; i++)
+            {
+                firstRacerTime += numbers[i];
+
+                if (numbers[i] == 0)
+                {
+                    firstRacerTime -= firstRacerTime * 0.2;
+                }
+            }
+
+            for (int i = numbers.Count - 1; i > midIdx; i--)
+            {
+                secondRacerTime += numbers[i];
+
+                if (numbers[i] == 0)
+                {
+                    secondRacerTime -= secondRacerTime * 0.2;
+                }
+            }
+
+            if (firstRacerTime < secondRacerTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {Math.Round(firstRacerTime, 1)}");
+            }
+            else 
+            {
+                Console.WriteLine($"The winner is right with total time: {Math.Round(secondRacerTime, 1)}");
+            }
+        }
+    }
+}
