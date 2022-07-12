dee2b25f6f0642e2e497f6557b2414612e9cd771
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-12 17:45:04 +0300

    Add files via upload

---
+++ 07_ObjectsAndClasses/Lab/01_RandomizeWords/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01_RandomizeWords
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                var randomIndex = random.Next(0, words.Length);
+
+                string a = words[randomIndex];
+                string b = words[i];
+
+                words[randomIndex] = b;
+                words[i] = a;
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ 07_ObjectsAndClasses/Lab/02_BigFactorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02_BigFactorial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger result = Factorial(n);
+            Console.WriteLine(result);
+        }
+
+        private static BigInteger Factorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
