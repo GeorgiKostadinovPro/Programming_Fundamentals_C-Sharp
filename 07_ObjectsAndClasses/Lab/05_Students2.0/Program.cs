f7bc3e2cf747fe213ed90ae0c06333c0f5db9d22
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-12 17:46:41 +0300

    Add files via upload

---
+++ 07_ObjectsAndClasses/Lab/05_Students2.0/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Students02
+{
+    public class Student
+    {
+        public Student(string firstName, string lastName, int age, string homeTown)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.HomeTown = homeTown;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string HomeTown { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string line = string.Empty;
+
+            while ((line = Console.ReadLine()) != "end")
+            {
+                string[] studentInfo = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string firstName = studentInfo[0];
+                string lastName = studentInfo[1];
+                int age = int.Parse(studentInfo[2]);
+                string homeTown = studentInfo[3];
+
+                Student student = students
+                    .FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+
+                if (student != null)
+                {
+                    student.FirstName = firstName;
+                    student.LastName = lastName;
+                    student.Age = age;
+                    student.HomeTown = homeTown;
+                }
+                else 
+                { 
+                    student = new Student(firstName, lastName, age, homeTown);
+                    students.Add(student);
+                }
+            }
+
+            string town = Console.ReadLine();
+
+            students = students.Where(s => s.HomeTown == town).ToList();
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+    }
+}
