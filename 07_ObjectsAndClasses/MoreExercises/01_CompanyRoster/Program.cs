d08513970ac7974b4f4ac2e281eb4294ff5ccccf
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-14 17:24:39 +0300

    Add files via upload

---
+++ 07_ObjectsAndClasses/MoreExercises/01_CompanyRoster/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_CompanyRoster
+{  
+    public class Department
+    {
+        public Department(string name)
+        {
+            this.Name = name;
+
+            this.Employees = new List<Employee>();
+        }
+        public string Name { get; set; }
+
+        public List<Employee> Employees { get; set; }
+    }
+
+    public class Employee
+    {
+        public Employee(string name, decimal salary, string department)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Department = department;
+        }
+
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public string Department { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); 
+            List<Department> departments = new List<Department>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] employeeInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = employeeInfo[0];
+                decimal salary = decimal.Parse(employeeInfo[1]);
+                string departmentName = employeeInfo[2];
+
+                Department department = departments.FirstOrDefault(d => d.Name == departmentName);
+
+                if (department == null)
+                {
+                    department = new Department(departmentName);
+                    departments.Add(department);
+                }
+
+                Employee employee = new Employee(name, salary, departmentName);
+                department.Employees.Add(employee);
+            }
+
+            Department bestDepartment = departments
+                .OrderByDescending(d => d.Employees.Average(e => e.Salary))
+                .FirstOrDefault();
+
+            Console.WriteLine($"Highest Average Salary: {bestDepartment.Name}");
+
+            foreach (var employee in bestDepartment.Employees.OrderByDescending(e => e.Salary))
+            {
+                Console.WriteLine($"{employee.Name} {employee.Salary:f2}");
+            }
+        }
+    }
+}
---
+++ 07_ObjectsAndClasses/MoreExercises/02_OldestFamilyMember/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_OldestFamilyMember
+{  
+    public class Family
+    {
+        private List<Person> people;
+        public Family()
+        {
+           this.people = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            if (!this.people.Contains(member))
+            {
+                this.people.Add(member);
+            }
+        }
+
+        public Person GetOldestMember()
+            => this.people
+            .OrderByDescending(p => p.Age)
+            .FirstOrDefault();
+    }
+
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); 
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Person oldestFamilyMember = family.GetOldestMember();
+            Console.WriteLine($"{oldestFamilyMember.Name} {oldestFamilyMember.Age}");
+        }
+    }
+}
