09db9d8fdfcf61e6990dddc2be8221d5abf61c38
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-15 13:49:16 +0300

    Add files via upload

---
+++ 08_BitwiseOperations/Exercises/03_P-thBit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03_PthBit
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int position = int.Parse(Console.ReadLine());
+
+            int bit = (number >> position) & 1;
+
+            Console.WriteLine(bit);
+        }
+    }
+}
