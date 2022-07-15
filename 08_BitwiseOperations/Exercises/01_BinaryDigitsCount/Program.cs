50fc1b0045d0530e4e27c703a946d82a3a194606
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-15 13:48:54 +0300

    Add files via upload

---
+++ 08_BitwiseOperations/Exercises/01_BinaryDigitsCount/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01_BinaryDigitsCount
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            char digit = char.Parse(Console.ReadLine());
+            
+            string binaryNumber = Convert.ToString(number, 2);
+            int digitCount = 0;
+
+            for (int i = 0; i < binaryNumber.Length; i++)
+            {
+                if (binaryNumber[i] == digit)
+                {
+                    digitCount++;
+                }
+            }
+
+            Console.WriteLine(digitCount);
+        }
+    }
+}
---
+++ 08_BitwiseOperations/Exercises/02_BitAtPosition1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02_BitAtPosition1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            string binaryNumber = Convert.ToString(number, 2);
+            int searchedBit = 0;
+
+            for (int i = binaryNumber.Length - 1; i >= 0; i--)
+            {
+                if (i == binaryNumber.Length - 2)
+                {
+                    searchedBit = binaryNumber[i] - '0';
+                    break;
+                }
+            }
+
+            Console.WriteLine(searchedBit);
+        }
+    }
+}
