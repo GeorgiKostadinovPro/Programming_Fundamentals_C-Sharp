5b71276aacdc8f7a18d520484aac7d75fb53b537
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-15 13:49:55 +0300

    Add files via upload

---
+++ 08_BitwiseOperations/Exercises/06_Tri-bitSwitch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06_TriBitSwitch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int position = int.Parse(Console.ReadLine());
+
+            int mask = 7 << position;
+            int result = number ^ mask;
+
+            Console.WriteLine(result);
+        }
+    }
+}
