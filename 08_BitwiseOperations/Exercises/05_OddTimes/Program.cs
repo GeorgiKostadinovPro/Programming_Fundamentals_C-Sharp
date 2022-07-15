73ebf3ddf9dc41a31ddb65ef41ede277bb0a4463
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-15 13:49:43 +0300

    Add files via upload

---
+++ 08_BitwiseOperations/Exercises/05_OddTimes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _05_OddTimes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int result = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result ^= numbers[i];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
