13ef127faefaa0e3db789c46622ef0d238087f24
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-16 17:11:44 +0300

    Add files via upload

---
+++ 09_AssociativeArrays/Lab/04_WordFilter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_WordFilter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            words = words
+                .Where(w => w.Length % 2 == 0)
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
