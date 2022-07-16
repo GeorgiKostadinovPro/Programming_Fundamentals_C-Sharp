da65b2532900a0582d65e513c565f81d8bd2896f
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-16 17:09:59 +0300

    Add files via upload

---
+++ 09_AssociativeArrays/Lab/02_OddOccurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_OddOccurrences
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> map = new Dictionary<string, int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i].ToLower();
+
+                if (!map.ContainsKey(word))
+                {
+                    map.Add(word, 0);
+                }
+
+                map[word]++; 
+            }
+
+            foreach (var word in map)
+            {
+                if (word.Value % 2 != 0)
+                {
+                    Console.Write($"{word.Key} ");
+                }
+            }
+        }
+    }
+}
