4560c636c6344eb8d5c5babee61182daf5017398
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-19 09:52:04 +0300

    Add files via upload

---
+++ 10_TextProcessing/Lab/05_DigitsLettersAndOther/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _05_DigitsLettersAndOther
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+             string str = Console.ReadLine();
+
+             string digits = string.Empty;
+             string letters = string.Empty;
+             string other = string.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char currChar = str[i];
+
+                if (char.IsDigit(currChar))
+                {
+                    digits += currChar;
+                }
+                else if (char.IsLetter(currChar))
+                {
+                    letters += currChar;
+                }
+                else 
+                {
+                    other += currChar;
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(other);
+        }
+    }
+}
