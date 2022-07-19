7397c472029fea944988642cdd071da5fd2f50d7
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-19 09:51:30 +0300

    Add files via upload

---
+++ 10_TextProcessing/Lab/02_RepeatStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace _02_RepeatStrings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                string currentElement = strings[i];
+                int repetitionCount = currentElement.Length;
+
+                for (int j = 0; j < repetitionCount; j++)
+                {
+                    sb.Append(currentElement);
+                }
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+    }
+}
