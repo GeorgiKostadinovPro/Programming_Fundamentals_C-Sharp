ded63cb4c31c64a34c47732ebf68c395a0cb0d08
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-19 09:50:51 +0300

    Add files via upload

---
+++ 10_TextProcessing/Lab/01_ReverseStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_ReverseStrings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string line = string.Empty;
+
+            while ((line = Console.ReadLine()) != "end") 
+            {
+                string str = line;
+                string reversedStr = string.Empty;
+
+                for (int i = str.Length - 1; i >= 0; i--)
+                {
+                    reversedStr += str[i];
+                }
+
+                Console.WriteLine($"{str} = {reversedStr}");
+            }
+        }
+    }
+}
