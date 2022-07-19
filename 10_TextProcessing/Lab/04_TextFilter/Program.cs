e32bcc630cf2712a79bcbbb9f96a67bcbb58d635
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-19 09:51:54 +0300

    Add files via upload

---
+++ 10_TextProcessing/Lab/04_TextFilter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04_TextFilter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string text = Console.ReadLine();
+
+            foreach (var bannedWord in bannedWords)
+            {
+                while (text.Contains(bannedWord))
+                {
+                    text = text.Replace(bannedWord, new string('*', bannedWord.Length));
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
