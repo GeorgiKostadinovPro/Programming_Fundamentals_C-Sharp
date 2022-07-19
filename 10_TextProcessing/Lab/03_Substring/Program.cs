006dfdfa2423f025fab6cb900f5f37f203fb3430
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-19 09:51:42 +0300

    Add files via upload

---
+++ 10_TextProcessing/Lab/03_Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03_Substring
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string firstStr = Console.ReadLine();  
+            string secondStr = Console.ReadLine();
+
+            while (secondStr.Contains(firstStr))
+            {
+               secondStr = secondStr.Replace(firstStr, string.Empty);
+            }
+
+            Console.WriteLine(secondStr);
+        }
+    }
+}
