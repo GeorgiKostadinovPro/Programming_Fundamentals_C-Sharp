19de91f58abd6730257080d3905bf1b120e29ace
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-20 16:01:27 +0300

    Add files via upload

---
+++ 10_TextProcessing/Exercises/04_CaesarCipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04_CaesarCipher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string encryptedMessage = string.Empty;
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                int asciiCode = message[i];
+                asciiCode += 3;
+
+                encryptedMessage += Convert.ToChar(asciiCode);
+            }
+
+            Console.WriteLine(encryptedMessage);
+        }
+    }
+}
