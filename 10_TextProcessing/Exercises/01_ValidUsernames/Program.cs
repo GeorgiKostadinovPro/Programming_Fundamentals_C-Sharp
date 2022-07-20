19a61273dee971841330220a4a0f3b34882b54cb
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-20 16:00:23 +0300

    Add files via upload

---
+++ 10_TextProcessing/Exercises/01_ValidUsernames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01_ValidUsernames
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            Regex regex = new Regex("^[A-Za-z0-9-_]{3,16}$");
+
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                string currUsername = usernames[i];
+
+                if (regex.IsMatch(currUsername))
+                {
+                    Console.WriteLine(currUsername);
+                }
+            }
+        }
+    }
+}
---
+++ 10_TextProcessing/Exercises/02_CharacterMultiplier/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _02_CharacterMultiplier
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string firstStr = strings[0];
+            string secondStr = strings[1];
+
+            int sum = 0;
+
+            int length = firstStr.Length > secondStr.Length ? secondStr.Length : firstStr.Length;
+                
+            for (int i = 0; i < length; i++)
+            {
+                int firstStrChar = firstStr[i];
+                int secondStrChar = secondStr[i];
+
+                sum += firstStrChar * secondStrChar;
+            }
+
+            if (length == firstStr.Length)
+            {
+                for (int i = length; i < secondStr.Length; i++)
+                {
+                    sum += secondStr[i];
+                }
+            }
+            else if (length == secondStr.Length)
+            {
+                for (int i = length; i < firstStr.Length; i++)
+                {
+                    sum += firstStr[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
