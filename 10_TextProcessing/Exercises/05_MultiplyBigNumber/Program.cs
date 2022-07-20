081fba626f56e09de8c39d1b9e8dc897ca86e240
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-20 16:01:59 +0300

    Add files via upload

---
+++ 10_TextProcessing/Exercises/05_MultiplyBigNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace _05_MultiplyBigNumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string firstNumber = Console.ReadLine();
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            BigInteger firstNumberParsed = BigInteger.Parse(firstNumber);
+            BigInteger product = firstNumberParsed * secondNumber;
+
+            Console.WriteLine(product);
+        }
+    }
+}
