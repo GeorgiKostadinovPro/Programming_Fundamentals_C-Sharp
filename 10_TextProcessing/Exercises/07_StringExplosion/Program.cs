61205fe2331eac6aa73c6b6bc1f3ca525b664b0d
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-20 16:02:51 +0300

    Add files via upload

---
+++ 10_TextProcessing/Exercises/07_StringExplosion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07_StringExplosion
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int bomb = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (bomb > 0 && str[i] != '>')
+                {
+                    str = str.Remove(i, 1);
+                    bomb--;
+                    i--;
+                }
+                else if (str[i] == '>')
+                {
+                    bomb += str[i + 1] - '0';
+                }
+            }
+
+            Console.WriteLine(str);
+        }
+    }
+}
