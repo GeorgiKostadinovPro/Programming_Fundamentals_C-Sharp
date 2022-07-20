033555732cca7f529894652dfeade4a387cefb5a
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-20 16:02:15 +0300

    Add files via upload

---
+++ 10_TextProcessing/Exercises/06_ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06_ReplaceRepeatingChars
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string cutString = string.Empty;    
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] != str[i + 1])
+                {
+                    cutString += str[i];
+                }
+            }
+
+            cutString += str[str.Length - 1];
+            Console.WriteLine(cutString);
+        }
+    }
+}
