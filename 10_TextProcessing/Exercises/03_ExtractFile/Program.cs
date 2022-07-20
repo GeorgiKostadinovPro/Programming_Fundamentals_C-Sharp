30fadfe5262b95fd845f9dacbb1a5df326365826
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-20 16:01:13 +0300

    Add files via upload

---
+++ 10_TextProcessing/Exercises/03_ExtractFile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03_ExtractFile
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] fileExtension = Console.ReadLine()
+                .Split('\\', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] fileData = fileExtension[fileExtension.Length - 1]
+                .Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+            string fileName = fileData[0];
+            string extension = fileData[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
