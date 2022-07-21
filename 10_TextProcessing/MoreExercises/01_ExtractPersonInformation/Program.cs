47d3e9a2a4fc503e1d977419a49347c4a83063a5
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-21 14:45:21 +0300

    Add files via upload

---
+++ 10_TextProcessing/MoreExercises/01_ExtractPersonInformation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01_ExtractPersonInformation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string matchName = @"@([A-Z{,1}a-z]+)\|";
+            string matchAge = @"#([0-9]+)\*";
+
+            for (int i = 0; i < n; i++)
+            {
+                string personInfo = Console.ReadLine();
+                Match nameMatch = Regex.Match(personInfo, matchName);
+                Match ageMatch = Regex.Match(personInfo, matchAge);
+
+                string personName = nameMatch.Groups[1].ToString();
+                string personAge = ageMatch.Groups[1].ToString();
+
+                Console.WriteLine($"{personName} is {personAge} years old.");
+            }
+        }
+    }
+}
---
+++ 10_TextProcessing/MoreExercises/02_AsciiSumator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02_AsciiSumator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int currCharValue = input[i];
+
+                if (currCharValue > (int)firstChar && currCharValue < (int)secondChar)
+                {
+                    sum += currCharValue;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
