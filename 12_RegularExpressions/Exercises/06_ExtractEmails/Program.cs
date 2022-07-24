676f790ae53452bacdf6b22f32a5ab8b783d74d2
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-24 13:36:27 +0300

    Add files via upload

---
+++ 12_RegularExpressions/Exercises/06_ExtractEmails/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06_ExtractEmails
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-_][A-Za-z]+)+))(\b|(?=\s))";
+
+            MatchCollection collection = Regex.Matches(input, pattern);
+
+            foreach (var email in collection)
+            {
+                Console.WriteLine(email);
+            }
+        }
+    }
+}
