ce86127b09eab20789438e16e2117d3a2ca6c096
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-24 13:34:35 +0300

    Add files via upload

---
+++ 12_RegularExpressions/Exercises/01_Furniture/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01_Furniture
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Regex regex = new Regex(@">>(?<name>[A-Za-z]+)<<(?<price>[0-9]+.?[0-9]*)!(?<quantity>[0-9]+)");
+
+            string line = string.Empty;
+
+            var furniture = new List<string>();
+
+            double price = 0.0;
+
+            while ((line = Console.ReadLine()) != "Purchase")
+            {
+                Match match = regex.Match(line);
+
+                if (match.Success)
+                {
+                    furniture.Add(match.Groups["name"].ToString());
+                    price += double.Parse(match.Groups["price"].ToString()) * double.Parse(match.Groups["quantity"].ToString());
+                }
+            }
+
+            Console.WriteLine("Bought furniture:");
+
+            foreach (var name in furniture)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine($"Total money spend: {price:f2}");
+        }
+    }
+}
---
+++ 12_RegularExpressions/Exercises/02_Race/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Race
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Distance { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> participantsList = new List<Person>();
+
+            string[] participants = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string line = Console.ReadLine();
+
+            while (line != "end of race")
+            {
+                string name = string.Empty;
+                int distance = 0;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    char curr = line[i];
+
+                    if (char.IsLetter(curr))
+                    {
+                        name += curr;
+                    }
+
+                    if (char.IsDigit(curr))
+                    {
+                        distance += curr - '0';
+                    }
+                }
+
+                Person person = null;
+
+                if (participants.Contains(name))
+                {
+                    if (participantsList.Any(p => p.Name == name))
+                    {
+                        person = participantsList.FirstOrDefault(p => p.Name == name);
+
+                        participantsList.Remove(person);
+
+                        person.Distance += distance;
+
+                        participantsList.Add(person);
+                    }
+                    else
+                    {
+                        person = new Person()
+                        {
+                            Name = name,
+                            Distance = distance,
+                        };
+
+                        participantsList.Add(person);
+                    }
+
+                }
+
+                line = Console.ReadLine();
+            }
+
+            participantsList = participantsList
+                .OrderByDescending(p => p.Distance)
+                .Take(3)
+                .ToList();
+
+            Console.WriteLine($"1st place: {participantsList[0].Name}");
+            Console.WriteLine($"2nd place: {participantsList[1].Name}");
+            Console.WriteLine($"3rd place: {participantsList[2].Name}");
+        }
+    }
+}
