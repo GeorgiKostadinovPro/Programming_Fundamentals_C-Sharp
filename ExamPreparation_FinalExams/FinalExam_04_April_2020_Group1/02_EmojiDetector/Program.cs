bf701939400ab2df85564bf689e727705cb6e5dc
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-27 14:50:41 +0300

    Update Program.cs

--- ExamPreparation_FinalExams/FinalExam_04_April_2020_Group1/02_EmojiDetector/Program.cs
+++ ExamPreparation_FinalExams/FinalExam_04_April_2020_Group1/02_EmojiDetector/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace EmojiDetector
+namespace _02_EmojiDetector
 {
     public class Program
     {
